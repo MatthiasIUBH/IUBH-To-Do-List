a06d40c38ea33d264de424c01630b860d1aa5470
Matthias.will@iubh-fernstudium.de <Studium2015>
2017-02-02 21:10:03 +0100

    ListView eingebaut, Datenbank "Tasks " ergänzt, Datenbank " Users"erweitert

--- app/src/main/java/com/example/m21219/logintest/MainActivity.java
+++ app/src/main/java/com/example/m21219/logintest/MainActivity.java
@@ -15,7 +15,7 @@ import static android.R.attr.name;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String KEY = "HEY" ;
+    public static final String UserID = "";
     SQLiteDatabase myDB=null;
 
     @Override
@@ -23,14 +23,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         //SQLiteDatabase myDB= null;
-
+         //SQLiteDatenbank anlegen und mit Usern und Tasks füllen
         try {
-            myDB = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
-            myDB.execSQL("CREATE TABLE IF NOT EXISTS Users (name VARCHAR, password VARCHAR)");
-            myDB.execSQL("INSERT INTO Users (name, password) VALUES ('Matthias', 'test1')");
-            myDB.execSQL("INSERT INTO Users (name, password) VALUES ('Hakan', 'test2')");
-            myDB.execSQL("INSERT INTO Users (name, password) VALUES ('Lukas', 'test3')");
+            myDB = this.openOrCreateDatabase("IUBHtoDoApp", MODE_PRIVATE, null);
+            myDB.execSQL("CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY,name VARCHAR, password VARCHAR)");
+            myDB.execSQL("INSERT INTO Users (id, name, password) VALUES (NULL,'Matthias', 'test1')");
+            myDB.execSQL("INSERT INTO Users (id, name, password) VALUES (NULL,'Hakan', 'test2')");
+            myDB.execSQL("INSERT INTO Users (id, name, password) VALUES (NULL,'Lukas', 'test3')");
+            myDB.execSQL("CREATE TABLE IF NOT EXISTS Tasks (id INTEGER PRIMARY KEY,name VARCHAR, description VARCHAR, completetiondate NUMERIC, completionstatus INTEGER, favorite INTEGER, userid INTEGER)");
+            myDB.execSQL("INSERT INTO Tasks (id, name, description, userid) VALUES (NULL,'Task1', 'Das ist Task1',1)");
+            myDB.execSQL("INSERT INTO Tasks (id, name, description, userid) VALUES (NULL,'Task2', 'Das ist Task2',1)");
+
         }
         catch(Exception e) {
             Log.e("Error", "Error", e);
@@ -48,27 +51,28 @@ public class MainActivity extends AppCompatActivity {
                 String password=editTextPassword.getText().toString();
 
                 // fetch the Password form database for respective user name
-                Cursor cursor = myDB.rawQuery("SELECT password FROM Users where name='"+userName.trim()+"'", null);
+                Cursor cursor = myDB.rawQuery("SELECT id, password FROM Users where name='"+userName.trim()+"'", null);
 
                 try {
                     if(cursor.getCount()<1) // UserName Not Exist
                     {
                         cursor.close();
-                        Toast toast = Toast.makeText(getApplicationContext(), "Invalid Username", 3);
+                        Toast toast = Toast.makeText(getApplicationContext(), "Unbekannter Username", 3);
                         toast.show();
                     }
 
                     cursor.moveToFirst();
                     String strPassword= cursor.getString(cursor.getColumnIndex("password"));
+                    String intUserID = cursor.getString(cursor.getColumnIndex("id"));
                     cursor.close();
 
                     // check if the Stored password matches with  Password entered by user
                     if(password.equals(strPassword))
                     {
-                        Toast.makeText(getApplicationContext(), "Congrats: Login Successfull", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "Login Erfolgreich!", Toast.LENGTH_LONG).show();
+
                         Intent myIntent = new Intent(MainActivity.this, MainView.class);
-                        myIntent.putExtra(KEY, editTextUserName.getText().toString());
-                        //myIntent.putExtra("key", value); //Optional parameters
+                        myIntent.putExtra(UserID, intUserID);
                         MainActivity.this.startActivity(myIntent);
                     }
                     else
--- app/src/main/java/com/example/m21219/logintest/MainView.java
+++ app/src/main/java/com/example/m21219/logintest/MainView.java
@@ -1,13 +1,23 @@
 package com.example.m21219.logintest;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainView extends AppCompatActivity {
 
-
+    SQLiteDatabase myDB=null;
+    private ListView TaskListView;
+    private ArrayAdapter<String> listAdapter ;
 
 
     @Override
@@ -16,12 +26,44 @@ public class MainView extends AppCompatActivity {
         setContentView(R.layout.activity_main_view);
 
         TextView message = (TextView) findViewById(R.id.message);
-        message.setText("Willkommen " + getIntent().getExtras().getString(MainActivity.KEY)+"!");
+        message.setText("Willkommen " + getIntent().getExtras().getString(MainActivity.UserID)+"!");
 
+        myDB = this.openOrCreateDatabase("IUBHtoDoApp", MODE_PRIVATE, null);
+        Cursor cursor = myDB.rawQuery("SELECT id, name, description FROM Tasks where userid='"+getIntent().getExtras().getString(MainActivity.UserID)+"'", null);
 
+        try {
+            if(cursor.getCount()<1) //wenn keine Tasks gefunden wurden!
+            {
+                cursor.close();
+                Toast toast = Toast.makeText(getApplicationContext(), "keine Tasks gefunden!", 3);
+                toast.show();
+            }
+            else
+            {
+                TaskListView = (ListView) findViewById(R.id.ListView_Tasks);
+                ArrayList<String> TaskList = new ArrayList<String>();
 
+                //Sqlite Cursor iterieren und in ein Array schreiben
+                cursor.moveToFirst();
+                while (cursor.moveToNext()) {
+                    TaskList.add(cursor.getString(cursor.getColumnIndex("description")));
+                }
 
+                //Array ausgeben in einem Listview
+                listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, TaskList);
+                TaskListView.setAdapter(listAdapter);
 
-
+            }
+        } catch (Exception e) {
+            // exception handling
+        } finally {
+            if(cursor != null){
+                cursor.close();
+            }
+        }
     }
 }
+
+
+
+
